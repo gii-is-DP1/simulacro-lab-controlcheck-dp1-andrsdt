c4fcfe59f88ba96018945cc5a7759e0fcf612985
andrsdt <andresdt@pm.me>
2021-12-21 10:54:26 +0100

    add: test 1

--- src/main/java/org/springframework/samples/petclinic/product/Product.java
+++ src/main/java/org/springframework/samples/petclinic/product/Product.java
@@ -1,13 +1,29 @@
 package org.springframework.samples.petclinic.product;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Product {
+    @Id
     Integer id;
+
+    @NotNull // TODO redundant?
+    @Length(min = 3, max = 50)
     String name;
+
+    @NotNull // TODO redundant?
+    @Min(0)
     double price;
+
     ProductType productType;
 }
