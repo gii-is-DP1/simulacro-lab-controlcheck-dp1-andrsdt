d8694a3dbca8827dcb11684c81cefe119338e048
andrsdt <andresdt@pm.me>
2022-01-13 17:39:30 +0100

    fix: test 1

--- src/main/java/org/springframework/samples/petclinic/product/Product.java
+++ src/main/java/org/springframework/samples/petclinic/product/Product.java
@@ -1,12 +1,12 @@
 package org.springframework.samples.petclinic.product;
 
 import javax.persistence.Entity;
-import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.PositiveOrZero;
 
 import org.hibernate.validator.constraints.Length;
+import org.springframework.samples.petclinic.model.BaseEntity;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -15,10 +15,7 @@ import lombok.Setter;
 @Setter
 @Entity
 @Table(name = "products")
-public class Product {
-    @Id
-    Integer id;
-
+public class Product extends BaseEntity {
     @Length(min = 3, max = 50)
     String name;
 
